6340b5fe910c97a849b63ae1ef2011a24d414226
MrAmitkumarSagar <130172389+MrAmitkumarSagar@users.noreply.github.com>
2023-05-26 17:03:12 +0530

    ready to

---
+++ src/Utils/Data/data.js
@@ -0,0 +1,47 @@
+//Dummy or dump Data only for testing purpose
+
+export const data = {
+  personal_info: {
+    first_name: "Amit",
+    last_name: "Kumar",
+    email: "MrAmitKumarSagar@gmail.com",
+    mobile: "8896715888",
+    address: "TajMahal",
+    city: "Agra",
+    state: "Uttar Pradesh",
+    postal_code: "282001",
+    objective:
+      "To learn develop applications  with knowledge of MERN stack development at ALMABETTER",
+  },
+  work_experience: [
+    {
+      id: 1,
+      job_title: "Recruit Manager",
+      organization_name: "Alma Data Scientist",
+      start_year: "2023",
+      end_year: "2050",
+    },
+    {
+      id: 2,
+      job_title: "HR / Customer Relationship Manager",
+      organization_name: "Alma Better",
+      start_year: "2023",
+      end_year: "2050",
+    },
+    {
+      id: 3,
+      job_title: "Sales Manager",
+      organization_name: "Alma X",
+      start_year: "2023",
+      end_year: "2050",
+    },
+  ],
+  education_details: {
+    type: "Graduation",
+    university: "HBTU Kanpur",
+    degree: "B.Tech",
+    start_year: "2012",
+    end_year: "2016",
+  },
+  key_skills: ["C","JAVA","C#","MERN Stack","SQL" ,"Swift", "Python"],
+};
